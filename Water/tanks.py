3fabcfa6b027d04f7f8295d69985c569e0169ab5
hasemar <ryan.haseman@stmartin.edu>
2018-02-17 20:53:57 -0800

    fixing tanks.py.  There is a problem with the horizontal level function. it is producing the wrong answer in the single output mode, I have no idea why

--- Water/tanks.py
+++ Water/tanks.py
@@ -1,5 +1,5 @@
 from math import pi, acos, sqrt
-import numpy as np
+from numpy import linspace
 
 c = 7.48052  # gal per cuft conversion
 
@@ -7,115 +7,132 @@ def get_gals(vol):
     gals = c * vol
     return gals
 
-def horizontal_vol(L, R):
-    h_arr = np.linspace(0, 2*R, 100)
-    vol = []
-    
-    for h in h_arr:
-        vol = L*(R**2 * acos((R-h)/R) - (R-h) * sqrt(2*R*h - h^2))
-        
-    vol_lookup = dict(zip(h_arr, vol))
-    
-    return vol_lookup
+def horizontal_vol(L, R, height=None):
+    if height:
+        h = height 
+        v = L*(R**2 * acos((R-h)/R) - (R-h) * sqrt(2*R*h - h**2))
+        return v
+    else: # create a height lookup dictionary
+        h_arr = linspace(0, 2*R, 11)
+        vol = []
+        for h in h_arr:
+            vol.append(L*(R**2 * acos((R-h)/R) - (R-h) * sqrt(2*R*h - h**2)))
+            
+        vol_lookup = dict(zip(vol, h_arr))
+        print vol
+        print h_arr
+        return vol_lookup
 
 class Tank:
     '''
-    Tank(**kwargs)
-        Tank Object arugments can be passed as a dict
+        Tank Object arugments can be passed as a dict:
+        example: 
+
         tank_data = {          
             'name' : 'string',
             'diameter' : int/float,
-            'height' : int/float,
+            'height' : int/float (default 0),
+            'length' = int/float (default 0)
             'freeboard' : int/float (default 0),
             'deadstorage' : int/float (default 0),
             'elevation' : int/float (default 0)
             'shape' : 'string' (default vertical)
             }
-        or
-        Tank(name='string',diameter=int/float, height=int/float )
+        Tank(**kwargs)
+
+        or individually like so:
+
+        Tank(name='string', diameter=int/float, height=int/float )
     '''
     
     def __init__(self, **kwargs):
         self.name = kwargs.get('name')
         self.diameter = kwargs.get('diameter')
-        self.height = kwargs.get('height')
+        self.height = kwargs.get('height', 0)
         self.freeboard = kwargs.get('freeboard', 0)
         self.deadstorage = kwargs.get('deadstorage', 0)
         self.elevation = kwargs.get('elevation', 0)
         self.shape = kwargs.get('shape', 'vertical')
-        self.length = 0
+        self.length = kwargs.get('length', 0)
 
     @property
     def area(self):
         r = self.diameter / 2
         return pi * r*r
-        
-    @property
-    def gal_per_ft(self):
-        return self.area * c
     
     @property
     def vol(self):
         if self.shape == 'horizontal':
-            
-            v_dict = horizontal_vol(self.length, self.diameter/2)
-            return v_dict[max(v_dict)]
-
+            return get_gals(horizontal_vol(self.length, self.diameter/2, self.diameter))
         else:
-            return self.gal_per_ft * self.height
+            return get_gals(self.area * self.height)
         
     @property
     def useable(self):
-        r = self.diameter/2
-        
         if self.shape == 'horizontal':
-
-            return 
+            r = self.diameter/2
+            v_dead = get_gals(horizontal_vol(self.length, r, self.deadstorage))
+            v_free = get_gals(horizontal_vol(self.length, r, self.freeboard))
+            return self.vol - v_dead - v_free
         else:
-            return (self.height - self.deadstorage - self.freeboard) * self.gal_per_ft
+            return get_gals((self.height - self.deadstorage - self.freeboard) * self.area)
    
     def getPercent(self, vol, of_vol):
          return vol/of_vol
 
     def getHeight(self, vol):
-        r = self.diameter/2
+        v = vol/c  # change back to cu.ft
+        print 'getHeight volume = ' + str(v) + 'ft^3'
         if self.shape == 'horizontal':
-            a_dead = r**2*(pi - acos((r-self.deadstorage)/r)) + 2*(r**2 - self.deadstorage * r)*(r - self.deadstorage)* sqrt(1 - ((r - self.deadstorage)/r)**2)/(r-self.deadstorage)
-            v_dead = a_dead *self.length
-            ft = np.arange(self.deadstorage, self.diameter, 0.01)
-            v_arr = []
-            for increment in ft:
-                if increment < self.diameter/2:
-                    v_arr.append((r**2*(pi - acos((r-increment)/r)) - 2*(r**2 - increment * r)*(r - increment)* sqrt(1 - ((r - increment)/r)**2)/(r-increment))*self.length)
-                else:
-                    v_arr.append((r**2*(pi - acos((r-increment)/r)) + 2*(r**2 - increment * r)*(r - increment)* sqrt(1 - ((r - increment)/r)**2)/(r-increment))*self.length)
-
-            h = find_nearest(v_arr, vol)
-                
+            v_dict = horizontal_vol(self.length, self.diameter/2)
+            h = v_dict[vol] if vol in v_dict else v_dict[min(v_dict.keys(), key=lambda k: abs(k-vol))]
+            return h
         else:
-            h = vol/self.gal_per_ft
+            h = vol/self.area
             return h
         
     def getInfo(self, SB=0, ES=0, OS=0, total_vol=0, details=False):
-        info = '''
-           {3:} \r\n
-           Base Elevation: {4:} ft \r\n
-           Orientation: {5:} \r\n
-           Tank Height: {6:} ft \r\n
-           Tank Diameter: {7:} ft \r\n
-           Tank cross-sectional area: {0:.1f} ft^2 \r\n
-           Total volume: {1:.1f} gal \r\n
-           Effective volume: {2:.1f} gal \r\n
-           '''.format(
-                      self.area,
-                      self.vol,
-                      self.useable,
-                      self.name,
-                      self.elevation,
-                      self.shape,
-                      self.height,
-                      self.diameter
-                     )
+        if self.shape == 'horizontal':
+            info = '''
+            {3:} \r\n
+            Base Elevation: {4:} ft \r\n
+            Orientation: {5:} \r\n
+            Tank Length: {6:} ft \r\n
+            Tank Diameter: {7:} ft \r\n
+            Tank Cross-Sectional Area: {0:.1f} ft^2 \r\n
+            Total Volume: {1:.1f} gal \r\n
+            Effective Volume: {2:.1f} gal \r\n
+            '''.format(
+                        self.area,
+                        self.vol,
+                        self.useable,
+                        self.name,
+                        self.elevation,
+                        self.shape,
+                        self.length,
+                        self.diameter
+                        )
+        else:
+             info = '''
+            {3:} \r\n
+            Base Elevation: {4:} ft \r\n
+            Orientation: {5:} \r\n
+            Tank Height: {6:} ft \r\n
+            Tank Diameter: {7:} ft \r\n
+            Tank cross-sectional area: {0:.1f} ft^2 \r\n
+            Total volume: {1:.1f} gal \r\n
+            Effective volume: {2:.1f} gal \r\n
+            '''.format(
+                        self.area,
+                        self.vol,
+                        self.useable,
+                        self.name,
+                        self.elevation,
+                        self.shape,
+                        self.height,
+                        self.diameter
+                        )
+
         if details:
             perc = self.getPercent(self.useable, total_vol)
             info +='''
@@ -141,22 +158,39 @@ class Tank:
 if __name__=='__main__':
     print "Test Script"
     
-    tank_data = {          
-        'name' : 'test',
-        'diameter' : 10,
-        'height' : 20,
+    horiz_tank_data = {      
+        'name' : 'horizontal tank',
+        'diameter' : 20,
+        'length' : 20,
         'freeboard' : 1,
-        'deadstorage' : 1,
-        'elevation' : 100,
+        'deadstorage' : 2,
+        'elevation' : 157,
         'shape' : 'horizontal'
         }
 
-    tank = Tank(**tank_data)
-    tank.length = 20
-    total = 2*tank.vol
-    
-    tank.getInfo(SB=100, ES=100, OS=100, total_vol=total, details=True)
+    vert_tank_data = {      
+        'name' : 'vertical tank',
+        'diameter' : 20,
+        'height' : 22,
+        'freeboard' : 1,
+        'deadstorage' : 2,
+        'elevation' : 157,
+        'shape' : 'vertical'
+        }
 
-    
+    h_tank = Tank(**horiz_tank_data)
+    v_tank = Tank(**vert_tank_data)
 
+    total = v_tank.vol + h_tank.vol
 
+    dead_vol = horizontal_vol(h_tank.length, h_tank.diameter/2, h_tank.deadstorage)
+    num_vol = horizontal_vol(20, 10, 2)
+    vols = horizontal_vol(h_tank.length, h_tank.diameter/2)
+    print vols
+    print h_tank.deadstorage
+    print h_tank.length
+    print h_tank.diameter/2
+    print dead_vol
+    print num_vol
+    #tank.getInfo()
+   # tank.getInfo(SB=100, ES=100, OS=100, total_vol=total, details=True)
