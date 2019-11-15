851cb1b1c98d42c21816b35ad45a2033609b4bc2
hasemar <hasemar@gmail.com>
2019-11-14 16:08:37 -0800

    im an idiot

--- Water/tanks.py
+++ Water/tanks.py
@@ -41,7 +41,7 @@ class Tank:
         self.shape = kwargs.get('shape', 'vertical')        # 'vertical' 'horizontal' 'box'
         self.length = kwargs.get('length', 0)               # ft
         self.width = kwargs.get('width', 0)                 # ft
-        self.operational = kwargs.get('operational' 0)      # ft
+        self.operational = kwargs.get('operational', 0)      # ft
         self.equalizing = kwargs.get('equalizing', 0)       # ft
         self.standby = kwargs.get('standby', 0)             # ft 
         self.fire = kwargs.get('fireflow', 0)           # ft
